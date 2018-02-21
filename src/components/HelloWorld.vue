4f3333cb1ef77d525f217c58a5af46a8cb5102b0
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-02-21 17:55:23 -0300

    adding things

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+      <md-toolbar class="md-accent">
+      <h3 class="md-title">Accent</h3>
+    </md-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!--><style lang="scss" scoped>
+  .md-toolbar + .md-toolbar {
+    margin-top: 16px;
+  }
+</style><!-->
